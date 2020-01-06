0d8b5dc00b2a1301c6e119a68114a36173a2d5dd
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2020-01-05 19:39:26 -0700

    find actors by film id grabs actor list from db

--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.filmquery.database;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 
@@ -14,15 +15,15 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		DatabaseAccessorObject db = new DatabaseAccessorObject();
-		Actor h = db.findActorById(3);
+		List<Actor> h = db.findActorsByFilmId(3);
 		System.out.println(h);
 	}
 
 	public DatabaseAccessorObject() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-		} catch(ClassNotFoundException e) {
-			
+		} catch (ClassNotFoundException e) {
+
 		}
 	}
 
@@ -58,13 +59,14 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 	public Actor findActorById(int actorId) {
 		Actor actor = null;
 		String sql = "Select actor.id, actor.first_name, actor.last_name from actor where actor.id = ?";
-		try( Connection conn = DriverManager.getConnection(url, user, pass);
+		try (Connection conn = DriverManager.getConnection(url, user, pass);
 				PreparedStatement stmt = preparedAndBindStatement(conn, actorId, sql);
 				ResultSet rst = stmt.executeQuery();) {
-			if(rst.next()) {
-				actor = new Actor(rst.getInt("actor.id"), rst.getString("actor.first_name"), rst.getString("actor.last_name"));
+			if (rst.next()) {
+				actor = new Actor(rst.getInt("actor.id"), rst.getString("actor.first_name"),
+						rst.getString("actor.last_name"));
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -74,7 +76,20 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 
 	@Override
 	public List<Actor> findActorsByFilmId(int filmId) {
-		List<Actor> actors = null;
+		List<Actor> actors = new ArrayList<Actor>();
+		Actor actor = null;
+		String sql = "Select actor.id, actor.first_name, actor.last_name from actor join film_actor on actor.id = film_actor.actor_id where film_actor.film_id = ?";
+		try (Connection conn = DriverManager.getConnection(url, user, pass);
+				PreparedStatement stmt = preparedAndBindStatement(conn, filmId, sql);
+				ResultSet rst = stmt.executeQuery();) {
+			while(rst.next()) {
+				actor = new Actor(rst.getInt("actor.id"), rst.getString("actor.first_name"), rst.getString("actor.last_name"));
+				actors.add(actor);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		return actors;
 	}
